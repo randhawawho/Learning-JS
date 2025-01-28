af56df7adda3a6cf460e2cb9f39a96e210d525d9
Bilal Randhawa <bilal.randhawa.avi@gmail.com>
2025-01-28 15:32:09 +0500

    test

--- 01_variables.js
+++ 01_variables.js
@@ -30,7 +30,27 @@ let userTwo = userOne
 
 userTwo.name = "Bilal Habib"
 
-console.table(userTwo);
+// console.table(userTwo);
+
+
+let newString = String("Bilal Habib")
+
+
+// console.log(newString.charAt(newString.length-1));
+
+
+let detail = "28 January 2025 Bilal Habib Python Killer"
+
+console.log(detail.slice(0, 15));
+console.log(detail.slice(15, 26));
+console.log(detail.slice(26));
+
+
+
+
+
+
+
 
 
 
