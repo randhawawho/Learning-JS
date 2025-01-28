d02a2a8932aa30f9bcd768db9d5a8e1ef016421e
Bilal Randhawa <bilal.randhawa.avi@gmail.com>
2025-01-28 15:20:06 +0500

    new code

---
+++ 01_variables.js
@@ -0,0 +1,37 @@
+const uid = 718239472374
+let name = "Bilal"
+let age = 25
+
+// console.table([uid, name, age]);
+
+
+
+
+let ageInNumber = Number(age)
+
+
+
+// console.log(ageInNumber);
+
+
+let userOne = {
+
+    name: "Bilal Randhawa",
+    email: "bilal.randhawa.avi@gmail.com",
+    age: 25
+
+
+}
+
+// console.log(userOne);
+
+let userTwo = userOne
+
+
+userTwo.name = "Bilal Habib"
+
+console.table(userTwo);
+
+
+
+
