8449fad3b2adf7ef387231cfda2cdd24c974daf2
Bilal Randhawa <bilal.randhawa.avi@gmail.com>
2025-01-28 18:38:52 +0500

    new

--- 01_variables.js
+++ 01_variables.js
@@ -4,15 +4,9 @@ let age = 25
 
 // console.table([uid, name, age]);
 
-
-
-
 let ageInNumber = Number(age)
 
-
-
 // console.log(ageInNumber);
-
 
 let userOne = {
 
@@ -20,33 +14,25 @@ let userOne = {
     email: "bilal.randhawa.avi@gmail.com",
     age: 25
 
-
 }
 
 // console.log(userOne);
 
 let userTwo = userOne
 
-
 userTwo.name = "Bilal Habib"
 
 // console.table(userTwo);
 
-
 let newString = String("Bilal Habib")
 
-
 // console.log(newString.charAt(newString.length-1));
-
 
 let detail = "28 January 2025 Bilal Habib Python Killer"
 
 // console.log(detail.slice(0, 15));
 // console.log(detail.slice(15, 26));
 // console.log(detail.slice(26));
-
-
-
 // console.log(`this is the date and name and pythhon killer ${detail}`);
 // tail}`);
 
