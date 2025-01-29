66747a96f1747f06f00991caecd2078f587c6c9b
Bilal Randhawa <bilal.randhawa.avi@gmail.com>
2025-01-29 13:57:58 +0500

    new changes

--- 01_variables.js
+++ 01_variables.js
@@ -1,57 +1,79 @@
-const uid = 718239472374
-let name = "Bilal"
-let age = 25
+// // const uid = 718239472374
+// // let name = "Bilal"
+// // let age = 25
 
-// console.table([uid, name, age]);
+// // // console.table([uid, name, age]);
 
-let ageInNumber = Number(age)
+// // let ageInNumber = Number(age)
 
-// console.log(ageInNumber);
+// // console.log(ageInNumber);
 
-let userOne = {
+// // let userOne = {
 
-    name: "Bilal Randhawa",
-    email: "bilal.randhawa.avi@gmail.com",
-    age: 25
+// //     name: "Bilal Randhawa",
+// //     email: "bilal.randhawa.avi@gmail.com",
+// //     age: 25
 
-}
+// // }
 
-// console.log(userOne);
+// // console.log(userOne);
 
-let userTwo = userOne
+// let userTwo = userOne
 
-userTwo.name = "Bilal Habib"
+// userTwo.name = "Bilal Habib"
 
-// console.table(userTwo);
+// // console.table(userTwo);
 
-let newString = String("Bilal Habib")
+// let newString = String("Bilal Habib")
 
-// console.log(newString.charAt(newString.length-1));
+// // console.log(newString.charAt(newString.length-1));
 
-let detail = "28 January 2025 Bilal Habib Python Killer"
+// let detail = "28 January 2025 Bilal Habib Python Killer"
 
-// console.log(detail.slice(0, 15));
-// console.log(detail.slice(15, 26));
-// console.log(detail.slice(26));
-// console.log(`this is the date and name and pythhon killer ${detail}`);
-// tail}`);
-
-
-// const number = [1,2,3,4]
-// number[100]=500
-// console.log(number);
+// // console.log(detail.slice(0, 15));
+// // console.log(detail.slice(15, 26));
+// // console.log(detail.slice(26));
+// // console.log(`this is the date and name and pythhon killer ${detail}`);
+// // tail}`);
 
 
-// console.log(typeof typeof 100);
-
-// const arr = [..."Bilal"]
-// console.log(arr);
-
-// console.log(parseInt("10+2"));
-// console.log(parseInt("7FM"));
-// console.log(parseInt("M7F"));
+// // const number = [1,2,3,4]
+// // number[100]=500
+// // console.log(number);
 
 
-// console.log(isNaN("7FM"));
-// console.log(isNaN("F7M"));
-// console.log(isNaN(10));
+// // console.log(typeof typeof 100);
+
+// // const arr = [..."Bilal"]
+// // console.log(arr);
+
+// // console.log(parseInt("10+2"));
+// // console.log(parseInt("7FM"));
+// // console.log(parseInt("M7F"));
+
+
+// // console.log(isNaN("7FM"));
+// // console.log(isNaN("F7M"));
+// // console.log(isNaN(10));
+
+
+
+
+// // let a = "The quick brown fox jump over the rop."
+// // let index = 4
+// // console.log(`The character code ${a.charCodeAt(index)} is equal to ${a.charAt(index)}`);
+
+
+// // let a = new String("I am a Sentence")
+// // console.log(a.valueOf(String));
+
+
+
+// // let a = "    I am a Sentence     "
+// // console.log(a.trimStart());
+
+
+// // let a = "    I am a Sentence     "
+// // console.log(`this is a demo sentence ${a.trim().toUpperCase()} to check the trim`);
+
+
